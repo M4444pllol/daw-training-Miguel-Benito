735b6951b55606d4fe3ebc36ad4efdd3d7d7170a
miguel <m4444pllol@gmail.com>
2023-11-28 16:23:44 +0100

    Java con explicacion añadido

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class copiar {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+	
+        System.out.println("Hola mundo, necesito aclarar algo: \n"
+        + "Maldito github cambio los terminos de uso de acceso a los tokens y mi gmail estaba puesto en pribado" + 
+        "\n por lo cual dava igual cuantas veces pusiera mi maldito token nunca iva a funcionar! >:| ");
+		
+		teclado.close();
+	} 
+}
